192512746e23ee2789d2fcf6a3bc0ac8bc3fad87
Liza K <liza.kosenuk@yandex.ru>
2020-01-07 19:57:37 +0300

    menu draw and update

--- buttons.py
+++ buttons.py
@@ -3,30 +3,35 @@ pygame.init()
 
 
 class Button:
-    def __init__(self, text, x, y, wid, hei):
+    def __init__(self, text, x, y, wid=300, hei=80, hovered=False):
         self.maincolor = pygame.Color(0, 97, 100)
         self.butcolor = pygame.Color(0, 129, 132)
         self.nohov = pygame.Color(255, 255, 255)
-        self.hov = pygame.Color(255, 255, 255)
+        self.hov = pygame.Color(200, 200, 200)
         self.text = text
         self.x = x
         self.y = y
         self.wid = wid
         self.hei = hei
+        self.hover = hovered
 
-    def draw(self, window, hov=False):
-        if hov:
+    def draw(self, window):
+        font = pygame.font.Font(None, 30)
+        if self.hover:
             pygame.draw.rect(window, self.maincolor, (self.x, self.y, self.wid, self.hei), 0)
             pygame.draw.rect(window, self.hov, (self.x - 3, self.y - 3, self.wid + 6, self.hei + 6), 3)
-            phrase = pygame.font.render(self.text, 1, (0, 0, 0))
+            phrase = font.render(self.text, 1, (255, 255, 255))
             window.blit(phrase, (self.x + (self.wid / 2 - phrase.get_width() / 2),
-                            self.y + (self.hei / 2 - phrase.get_height() / 2)))
+                                 self.y + (self.hei / 2 - phrase.get_height() / 2)))
         else:
             pygame.draw.rect(window, self.butcolor, (self.x, self.y, self.wid, self.hei), 0)
             pygame.draw.rect(window, self.nohov, (self.x - 3, self.y - 3, self.wid + 6, self.hei + 6), 3)
-            phrase = pygame.font.render(self.text, 1, (0, 0, 0))
+            phrase = font.render(self.text, 1, (255, 255, 255))
             window.blit(phrase, (self.x + (self.wid / 2 - phrase.get_width() / 2),
                                  self.y + (self.hei / 2 - phrase.get_height() / 2)))
+
+    def set_hov(self, state):
+        self.hover = state
 
     def onclick(self, pos):
         if self.x < pos[0] < self.x + self.wid:
--- mainbody.py
+++ mainbody.py
@@ -1,6 +1,23 @@
 import pygame, os, sys
-import buttons
+from buttons import Button
 pygame.init()
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(way, name, colorkey=None):
+    fullname = os.path.join(way, name)
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
 class MenuWindow:
@@ -9,11 +26,55 @@ class MenuWindow:
         self.butweight, self.butheight = 300, 80
         self.offset = 170
         self.space = 100
+        self.titlspace = 140
         self.clr = pygame.Color(0, 97, 100)
         self.run = True
 
     def mainmenu(self):
-        pass
+        screen = pygame.display.set_mode(self.size)
+        but_in_menu = []
+        continue_but = Button('Продолжить', self.offset, self.titlspace)
+        but_in_menu.append(continue_but)
+        new_g_but = Button('Новая игра', self.offset, self.titlspace + self.space)
+        but_in_menu.append(new_g_but)
+        ach_but = Button('Достижения', self.offset, self.titlspace + self.space * 2)
+        but_in_menu.append(ach_but)
+        pre_but = Button('Предыстория', self.offset, self.titlspace + self.space * 3)
+        but_in_menu.append(pre_but)
+        exit_but = Button('Выход', self.offset, self.titlspace + self.space * 4)
+        but_in_menu.append(exit_but)
+        header = load_image('bigpic', 'title.png')
+        while self.run:
+            screen.fill(self.clr)
+            screen.blit(header, (self.offset - 20, 11))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.run = False
+                    terminate()
+                if event.type == pygame.MOUSEMOTION:
+                    for i in but_in_menu:
+                        pos = pygame.mouse.get_pos()
+                        i.set_hov(i.onclick(pos))
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    if continue_but.onclick(pos):
+                        # продолжить игру
+                        pass
+                    if new_g_but.onclick(pos):
+                        # начать игру с первого уровня
+                        pass
+                    if ach_but.onclick(pos):
+                        # показать достижения
+                        pass
+                    if pre_but.onclick(pos):
+                        # показать пролог
+                        pass
+                    if exit_but.onclick(pos):
+                        terminate()
+            for i in but_in_menu:
+                i.draw(screen)
+            pygame.display.flip()
+
 
 
 class Prologue:
@@ -24,37 +85,27 @@ class Prologue:
         self.naming = 'P0.png'
         self.running = True
 
-    def terminate(self):
-        pygame.quit()
-        sys.exit()
-
-    def load_image(self, name, colorkey=None):
-        fullname = os.path.join('prolog', name)
-        image = pygame.image.load(fullname)
-        if colorkey is not None:
-            if colorkey == -1:
-                colorkey = image.get_at((0, 0))
-            image.set_colorkey(colorkey)
-        else:
-            image = image.convert_alpha()
-        return image
-
     def tellstory(self):
         screen = pygame.display.set_mode(self.size)
         while self.running:
-            fon = pygame.transform.scale(self.load_image(self.naming), (self.width, self.height))
+            fon = pygame.transform.scale(load_image('prolog', self.naming), (self.width, self.height))
             screen.blit(fon, (0, 0))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
-                    self.terminate()
+                    self.running = False
+                    terminate()
                 elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                     self.curr_cadr += 1
                     if self.curr_cadr > self.all_cadr:
-                        running = False
+                        self.running = False
                         return
                     else:
                         self.naming = 'P' + str(self.curr_cadr) + '.png'
-                        fon = pygame.transform.scale(self.load_image(self.naming), (self.width, self.height))
+                        fon = pygame.transform.scale(load_image('prolog', self.naming), (self.width, self.height))
                         screen.blit(fon, (0, 0))
             pygame.display.flip()
+
+
+men = MenuWindow()
+men.mainmenu()
+pygame.quit()
